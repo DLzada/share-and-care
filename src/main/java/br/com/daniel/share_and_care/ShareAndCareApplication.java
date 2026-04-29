dbc2d9c783ab17c0bd1a7937dc43d3fa32adb0f7
dlzada <danielluiz1607@gmail.com>
2026-04-29 09:03:00 -0300

    First commit of project

---
+++ src/main/java/br/com/daniel/share_and_care/ShareAndCareApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel.share_and_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareAndCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareAndCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daniel/share_and_care/ShareAndCareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.share_and_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareAndCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
