5dadc12c4ca213a6d6f196f62d4886a2d3d3acd9
dlzada <danielluiz1607@gmail.com>
2026-04-29 09:40:19 -0300

    feat: create migration of doadores

---
+++ src/main/resources/db/migration/V2__create_table_doadores.sql
@@ -0,0 +1,7 @@
+CREATE TABLE doadores (
+    id UUID PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    contato VARCHAR(100),
+    tipo_doador VARCHAR(20) NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
