a43fa0c3a25a096f118e655fc39cb176248858b7
dlzada <danielluiz1607@gmail.com>
2026-04-29 10:49:30 -0300

    feat: create Entity doador

---
+++ src/main/java/br/com/daniel/share_and_care/domain/Doador.java
@@ -0,0 +1,33 @@
+package br.com.daniel.share_and_care.domain;
+
+import br.com.daniel.share_and_care.domain.enums.TipoDoador;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "doadores")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Doador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_doador", nullable = false)
+    private TipoDoador tipoDoador;
+
+    @CreationTimestamp
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+}
