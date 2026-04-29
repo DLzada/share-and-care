0fe512f9d6d610c4370ff05d797cf5eb3066fc8d
dlzada <danielluiz1607@gmail.com>
2026-04-29 09:50:22 -0300

    feat: create migrations of estoque and movimentacoes

---
+++ src/main/resources/db/migration/V4__create_table_estoque.sql
@@ -0,0 +1,7 @@
+CREATE TABLE estoque(
+    id UUID PRIMARY KEY,
+    item_id UUID UNIQUE,
+    quantidade_atual INT NOT NULL DEFAULT 0,
+    ultima_atulizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_item_estoque FOREIGN KEY (item_id) REFERENCES itens(id)
+);
---
+++ src/main/resources/db/migration/V5__create_table_movimentacoes.sql
@@ -0,0 +1,11 @@
+CREATE TABLE movimentacoes(
+    id UUID PRIMARY KEY,
+    item_id UUID NOT NULL,
+    doador_id UUID,
+    tipo VARCHAR(20) NOT NULL,
+    quantidade INT NOT NULL,
+    data_movimentacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    observacao VARCHAR(255),
+    CONSTRAINT fk_item_movimentado FOREIGN KEY (item_id) REFERENCES itens(id)
+    CONSTRAINT fk_doador_movimentacao FOREIGN KEY (doador_id) REFERENCES doadores(id)
+);
