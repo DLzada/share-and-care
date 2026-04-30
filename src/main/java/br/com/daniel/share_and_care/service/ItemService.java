ec0510fee049efcdcdeb40a1b413535de09e2681
dlzada <danielluiz1607@gmail.com>
2026-04-30 08:30:26 -0300

    feat: create ItemService and methods of salvar and listarTodos

---
+++ src/main/java/br/com/daniel/share_and_care/service/ItemService.java
@@ -0,0 +1,34 @@
+package br.com.daniel.share_and_care.service;
+
+import br.com.daniel.share_and_care.domain.Categoria;
+import br.com.daniel.share_and_care.domain.Item;
+import br.com.daniel.share_and_care.dto.ItemRequestDTO;
+import br.com.daniel.share_and_care.repository.ItemRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+    private final CategoriaService categoriaService;
+
+    @Transactional
+    public Item salvar(ItemRequestDTO dto){
+        Categoria categoria = categoriaService.buscarPorId(dto.categoriaId());
+
+        Item item = new Item();
+        item.setNome(dto.nome());
+        item.setUnidadeMedida(dto.unidadeMedida());
+        item.setCategoria(categoria);
+
+        return itemRepository.save(item);
+    }
+
+    public List<Item> listarTodos(){
+        return itemRepository.findAll();
+    }
+}
