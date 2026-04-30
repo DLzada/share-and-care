1f3f2b0ae949c8597d9138288f70d0a32f7d31ba
dlzada <danielluiz1607@gmail.com>
2026-04-30 09:05:36 -0300

    feat: add methods of listarTodos and buscarPorId in DoadorService

--- src/main/java/br/com/daniel/share_and_care/service/DoadorService.java
+++ src/main/java/br/com/daniel/share_and_care/service/DoadorService.java
@@ -2,10 +2,14 @@ package br.com.daniel.share_and_care.service;
 
 import br.com.daniel.share_and_care.domain.Doador;
 import br.com.daniel.share_and_care.dto.DoadorRequestDTO;
+import br.com.daniel.share_and_care.infra.exception.BusinessException;
 import br.com.daniel.share_and_care.repository.DoadorRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -20,5 +24,14 @@ public class DoadorService {
         doador.setTipoDoador(dto.tipoDoador());
 
         return doadorRepository.save(doador);
+    }
+
+    public List<Doador> listarTodos(){
+        return doadorRepository.findAll();
+    }
+
+    public Doador buscarPorId(UUID id){
+        return doadorRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("O doador com o ID %s nao foi encontrado", id.toString()));
     }
 }
