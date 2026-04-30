35e72206b9baa421c55ca09879125c32531c757e
dlzada <danielluiz1607@gmail.com>
2026-04-30 17:00:55 -0300

    fix: add pageable in estoque

--- src/main/java/br/com/daniel/share_and_care/controller/EstoqueController.java
+++ src/main/java/br/com/daniel/share_and_care/controller/EstoqueController.java
@@ -3,23 +3,24 @@ package br.com.daniel.share_and_care.controller;
 import br.com.daniel.share_and_care.domain.Estoque;
 import br.com.daniel.share_and_care.service.EstoqueService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/estoque")
+@RequestMapping("/estoques")
 @AllArgsConstructor
 public class EstoqueController {
     private final EstoqueService estoqueService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Estoque> listarEstoqueCompleto(){
-        return estoqueService.listarEstoqueCompleto();
+    public Page<Estoque> listarEstoqueCompleto(@PageableDefault(size = 10, page = 0) Pageable paginacao){
+        return estoqueService.listarEstoqueCompleto(paginacao);
     }
 }
--- src/main/java/br/com/daniel/share_and_care/service/EstoqueService.java
+++ src/main/java/br/com/daniel/share_and_care/service/EstoqueService.java
@@ -3,16 +3,17 @@ package br.com.daniel.share_and_care.service;
 import br.com.daniel.share_and_care.domain.Estoque;
 import br.com.daniel.share_and_care.repository.EstoqueRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class EstoqueService {
     private final EstoqueRepository estoqueRepository;
 
-    public List<Estoque> listarEstoqueCompleto(){
-        return estoqueRepository.findAll();
+    public Page<Estoque> listarEstoqueCompleto(Pageable paginacao){
+        return estoqueRepository.findAll(paginacao);
     }
 }
