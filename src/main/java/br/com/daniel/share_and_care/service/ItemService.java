d34107076ac2005d2726370673309780fe5aab5a
dlzada <danielluiz1607@gmail.com>
2026-04-30 09:25:23 -0300

    feat: add method buscarPorId in ItemService

--- src/main/java/br/com/daniel/share_and_care/service/ItemService.java
+++ src/main/java/br/com/daniel/share_and_care/service/ItemService.java
@@ -3,12 +3,14 @@ package br.com.daniel.share_and_care.service;
 import br.com.daniel.share_and_care.domain.Categoria;
 import br.com.daniel.share_and_care.domain.Item;
 import br.com.daniel.share_and_care.dto.ItemRequestDTO;
+import br.com.daniel.share_and_care.infra.exception.BusinessException;
 import br.com.daniel.share_and_care.repository.ItemRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -30,5 +32,10 @@ public class ItemService {
 
     public List<Item> listarTodos(){
         return itemRepository.findAll();
+    }
+
+    public Item buscarPorId(UUID id){
+        return itemRepository.findById(id)
+                .orElseThrow(()-> new BusinessException("Item com ID %s não encontrado", id.toString()));
     }
 }
