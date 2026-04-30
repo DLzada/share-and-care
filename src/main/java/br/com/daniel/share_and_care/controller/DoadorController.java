520f6cef8533abe15d41d748deea918deaa574b1
dlzada <danielluiz1607@gmail.com>
2026-04-30 09:56:40 -0300

    fix: add @Valid in method post of DoadorController

--- src/main/java/br/com/daniel/share_and_care/controller/DoadorController.java
+++ src/main/java/br/com/daniel/share_and_care/controller/DoadorController.java
@@ -3,6 +3,7 @@ package br.com.daniel.share_and_care.controller;
 import br.com.daniel.share_and_care.domain.Doador;
 import br.com.daniel.share_and_care.dto.DoadorRequestDTO;
 import br.com.daniel.share_and_care.service.DoadorService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class DoadorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Doador salvar(@RequestBody DoadorRequestDTO dto){
+    public Doador salvar(@RequestBody @Valid DoadorRequestDTO dto){
         return doadorService.salvar(dto);
     }
 
