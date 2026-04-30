414322c5dee5a58c929ded5e626540d730c20614
dlzada <danielluiz1607@gmail.com>
2026-04-30 08:24:00 -0300

    fix: add import class item in Item Repository

--- src/main/java/br/com/daniel/share_and_care/repository/ItemRepository.java
+++ src/main/java/br/com/daniel/share_and_care/repository/ItemRepository.java
@@ -1,6 +1,6 @@
 package br.com.daniel.share_and_care.repository;
 
-import br.com.daniel.share_and_care.dto.ItemRequestDTO;
+import br.com.daniel.share_and_care.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
