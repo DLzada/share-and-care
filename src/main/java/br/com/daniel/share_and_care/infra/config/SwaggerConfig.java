729f4c4430bf06c17b59471df85cf1e6f68caadb
dlzada <danielluiz1607@gmail.com>
2026-05-01 13:24:25 -0300

    fix: add notation necessary in SecurityConfig

--- src/main/java/br/com/daniel/share_and_care/infra/config/SwaggerConfig.java
+++ src/main/java/br/com/daniel/share_and_care/infra/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package br.com.daniel.share_and_care.infra.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +16,12 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Share & Care API")
                         .description("API REST para gestão de doações e controle de estoque de alimentos.")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("basicAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
     }
 }
--- src/main/java/br/com/daniel/share_and_care/infra/security/SecurityConfig.java
+++ src/main/java/br/com/daniel/share_and_care/infra/security/SecurityConfig.java
@@ -1,22 +1,25 @@
 package br.com.daniel.share_and_care.infra.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
